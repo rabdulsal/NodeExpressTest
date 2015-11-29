28b6fe4a0645334337d7349cfd39a014a7e11745
Rashad Abdul-Salaam <rashad@getskinclick.com>
2015-11-29 00:22:49 -0600

    Route & functionality updates

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser');
+var parseUrlencoded = bodyParser.urlencoded({ extended:false });
+
 var logger = require('./logger');
 app.use(logger);
 
 app.use(express.static('public'));
+
+/* ---------- DATA ---------- */
 
 var blocks = {
 	'Fixed' : 'Fastened securely in position',
@@ -18,6 +23,8 @@ var locations = {
 	'Rotating' : 'Penthouse'
 }
 
+/* -------- PARAM HANDLERS ----------- */
+
 app.param('name', function(req, res, next) {
 	var name = req.params.name;
 	var block = name[0].toUpperCase() + name.slice(1).toLowerCase();
@@ -27,32 +34,50 @@ app.param('name', function(req, res, next) {
 	next();
 });
 
-app.get('/blocks', function(req, res) {
-	var blks = Object.keys(blocks);
-	if (req.query.limit >=0) {
-		res.json(blks.slice(0,req.query.limit));
-	} else {
-		res.json(blks);
-	}
-});
+/* ------------BLOCK ROUTES-----------*/
+
+app.route('/blocks')
+	.get(function(req, res) {
+		var blks = Object.keys(blocks);
+		if (req.query.limit >=0) {
+			res.json(blks.slice(0,req.query.limit));
+		} else {
+			res.json(blks);
+		}
+	})
+	.post(parseUrlencoded, function(req, res){
+		var newBlock = req.body;
+		blocks[newBlock.name] = newBlock.description;
+		res.status(201).json(newBlock.name);
+	});
+
+app.route('/blocks/:name')
+	.get(function(req,res){
+		var description = blocks[req.blockName];
+		if (!description) {
+			res.status(404).json('No description found for ' + req.params.name);
+		} else {
+			res.json(description);
+		}
+	})
+	.delete(function(req, res){
+		var block = blocks[req.blockName];
+		if (block) {
+			delete blocks[req.blockName];
+			res.sendStatus(200);
+		} else {
+			res.sendStatus(404);
+		}
+	});
+
+/* ----- LOCATION ROUTES ----------*/
 
 app.get('/locations/:name', function(req,res) {
 	var location = locations[req.blockName];
 	res.json(location);
 });
 
-app.get('/blocks/:name', function(req,res){
-	
-	var description = blocks[req.blockName];
-	if (!description) {
-		res.status(404).json('No description found for ' + req.params.name);
-	} else {
-		res.json(description);
-	}
-	/*
-	
-	*/
-});
+/* ---------- LISTENER ----------- */
 
 app.listen(3000, function(){
 	console.log('Listening on port 3000');
--- public/client.js
+++ public/client.js
@@ -4,9 +4,44 @@ $(function(){
 
 	function appendToList(blocks){
 		var list = [];
+		var content, block;
 		for(var i in blocks){
-			list.push($('<li>', { text: blocks[i] }));
+			block = blocks[i];
+			content = '<a href="/blocks/'+block+'">'+block+'</a> '+
+			'<a href="#" data-block="'+block+'"><img src="del.jpg"></a>';
+			list.push($('<li>', { html: content }));
 		}
 		$('.block-list').append(list);
 	}
+/* --------- CLICK EVENTS -----------*/
+
+	$('form').on('submit', function(event){
+		event.preventDefault();
+		var form = $(this);
+		var blockData = form.serialize();
+
+		$.ajax({
+			type: 'POST', 
+			url: '/blocks', 
+			data: blockData
+		}).done(function(blockName){
+			appendToList([blockName]);
+			form.trigger('reset');
+		});
+	});
+
+	$('.block-list').on('click', 'a[data-block]', function(event){
+		if(!confirm('Are you sure ?')){
+			return false;
+		} 
+
+		var target = $(event.currentTarget);
+
+		$.ajax({
+			type: 'DELETE', 
+			url: '/blocks/' + target.data('block')
+		}).done(function(){
+			target.parents('li').remove();
+		});
+	});
 });
